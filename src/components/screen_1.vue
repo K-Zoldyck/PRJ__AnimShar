6ba3df88b2abacd5acd8767b73d03e1ab1990959
devv-robert <mr.suida@outlook.com>
2024-08-14 21:17:12 -0300

    add: temporary cover default imagem

--- src/components/screen_1.vue
+++ src/components/screen_1.vue
@@ -5,8 +5,8 @@
 <template>
     <ul class="anime-grid-box">
         <li v-for="n in 20" class="anime-item">
-            <a href="#url"><img src="" alt="anime-cover"></a>
-            <span class="anime-data"> titulo do anime: 8.1 | <i class="anime-state">assistindo</i></span>
+            <a href="#url"><img src="./resources/default_cover.jpg" alt="anime-cover" class="item-cover"></a>
+            <span class="item-data"> titulo do anime: 8.1 | <i class="item-state">assistindo</i></span>
         </li>
     </ul>
 </template>
@@ -16,16 +16,30 @@
         display: flex;
         flex-wrap: wrap;
         justify-content: space-between;
+        gap: 10px;
         width: 100%;
         height: 90vh;
         overflow: auto;
         list-style: none;
+        /* padding-top: 2%; */
     }
         .anime-item {
             cursor: pointer;
             display: block;
-            width: 160px;
-            height: 240px;
-            margin: 2%;
+            width: 190px;
+            height: 270px;
+            box-shadow: 1px 1px 3px 2px #000;
+            /* margin: 1%; */
         }
+            .item-cover {
+                display: block;
+                width: 100%;
+                height: 240px;
+            }
+            .item-data {
+                display: block;
+                width: 100%;
+                background-color: #000;
+                color: var(--fgcolor);
+            }
 </style>
